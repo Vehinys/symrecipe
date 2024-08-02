86951bc2543310c8a583bece5486577a366b82cb
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-08-02 12:31:33 +0200

    creer entite + fixture

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 class Ingredient
 {
@@ -14,13 +16,28 @@ class Ingredient
     private ?int $id;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:2, max:50)]
     private ?string $name;
 
     #[ORM\Column]
+    #[Assert\NotNull()]
+    #[Assert\Positive()]
+    #[Assert\LessThan(200)]
     private ?float $price;
 
     #[ORM\Column]
+    #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt;
+
+
+    /**
+     *  Contructor
+     */
+    public function __construct()
+    {
+        $this-> createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
