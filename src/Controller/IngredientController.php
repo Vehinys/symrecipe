15f46d56a1cec09998d28d715e61352b3c61f10e
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-08-02 16:20:18 +0200

    MAJ FORM

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,15 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
+    /**
+     * this function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+
     #[Route('/ingredient', name: 'ingredient')]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator,Request $request): Response
     {
@@ -24,5 +37,17 @@ class IngredientController extends AbstractController
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
         ]);
+    }
+    
+    #[Route('/ingredient/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
+    public function new(): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+
     }
 }
