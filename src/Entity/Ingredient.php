0d739edda584703d29770956cbf68218271e2b07
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-08-14 17:50:48 +0200

    ADD REMOVE BUG

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -2,10 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use App\Repository\IngredientRepository;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 #[UniqueEntity('name')]
